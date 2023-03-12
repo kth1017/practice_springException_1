b3a61f480710c7a3154bbffb5f0bed51fb346120
kth <rnjsxo1017@gmail.com>
2023-03-13 00:32:45 +0900

    0312 끝

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,17 +1,34 @@
 package hello.exception.servlet;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@Slf4j
 @Controller
 public class ErrorPageController {
 
+    public static final String ERROR_EXCEPTION = "javax.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
+    public static final String ERROR_STATUS_CODE ="javax.servlet.error.status_code";
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest req, HttpServletResponse res){
+        printErrorInfo(req);
         return "error-page/500";
+
+    }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
+        log.info("dispatcherType={}", request.getDispatcherType());
+
+
 
     }
 
