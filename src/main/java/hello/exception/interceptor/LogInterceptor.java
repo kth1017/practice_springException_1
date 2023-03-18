8cec3ea2e0e428dc95f690427f5900644bf004ca
kth <rnjsxo1017@gmail.com>
2023-03-18 21:38:13 +0900

    0318 로그인 학습 끝

---
+++ src/main/java/hello/exception/interceptor/LogInterceptor.java
@@ -0,0 +1,51 @@
+package hello.exception.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+    public static final String LOG_ID = "logId";
+
+    // uuid를 여기 호출하면 싱글톤 깨짐 그래서 setAttribute로 request에 추가
+    // 부가설명 : 세 메소드의 호출 시점이 모두 분리되어 지역 변수 사용 불가
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        String uuid = UUID.randomUUID().toString();
+
+        request.setAttribute(LOG_ID, uuid);
+
+        //정적리소스 : ResourceHttpRequestHandler
+        //@RequestMapping : HandlerMethod
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler;
+        }
+
+        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String logId = (String) request.getAttribute(LOG_ID);
+
+        log.info("RESPONSE [{}][{}][{}]", logId, requestURI, handler);
+
+    }
+}
