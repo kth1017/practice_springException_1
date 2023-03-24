0be77ce7b40d0ef75ca191cb8522a690bb42dc8b
kth <rnjsxo1017@gmail.com>
2023-03-24 23:34:25 +0900

    0324:

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class ApiExceptionController {
-    @GetMapping("/api/members/{id}")
-    public MemberDto getMember(@PathVariable("id") String id) {
+
+//    @GetMapping("/api/members/{id}")
+    public MemberDto getMember1(@PathVariable("id") String id) {
 
         // 예외 발생시 api가 예외 html을 던짐 > x
         if (id.equals("ex")) {
@@ -19,6 +20,16 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello" + id);
+    }
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMember2(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        return new MemberDto(id, "hello " + id);
     }
 
     @Data
